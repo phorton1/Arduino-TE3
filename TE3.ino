7089e40df13dd827e6820713282a9dce3fba50e8
phorton1 <available@upon.request>
2024-05-31 09:48:42 +0000

    added a square wave signal generator to test opto isolation

--- TE3.ino
+++ TE3.ino
@@ -10,6 +10,19 @@
 #define WITH_ROTARY_BOARD  0
 
 
+#define SERIAL_ISOLATION_TEST	0   // 25
+	// A pin for testing the ESP32 serial opto-isolator.
+	// The program will generate a square wave signal on this pin.
+#define ISOLATION_FREQ          100000
+    // frequency for the square weave
+
+#if SERIAL_ISOLATION_TEST
+	#include <driver/ledc.h>
+#endif
+
+
+
+
 //--------------------------------------
 // rotary board
 //--------------------------------------
@@ -63,6 +76,36 @@ void setup()
 
     #if WITH_ROTARY_BOARD
         rotaryBoard::begin(PIN_ROTARY_INTERRUPT);
+    #endif
+
+    #if SERIAL_ISOLATION_TEST
+    
+        ledc_timer_config_t ledc_timer;
+        ledc_channel_config_t ledc_channel;
+
+        ledc_timer.speed_mode = LEDC_HIGH_SPEED_MODE;
+        ledc_timer.bit_num    = LEDC_TIMER_2_BIT;
+        ledc_timer.timer_num  = LEDC_TIMER_1;
+        ledc_timer.freq_hz    = 2048000;
+
+        ledc_channel.gpio_num   = SERIAL_ISOLATION_TEST;    // GPIO_NUM_33,
+        ledc_channel.speed_mode = LEDC_HIGH_SPEED_MODE;
+        ledc_channel.channel    = LEDC_CHANNEL_0;
+        ledc_channel.timer_sel  = LEDC_TIMER_1;
+        ledc_channel.duty       = 2;
+
+        ledc_timer_config(&ledc_timer);
+        ledc_channel_config(&ledc_channel);
+
+        uint32_t before_freq = ledc_get_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_1);
+        int rslt = ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_1, ISOLATION_FREQ);
+        delay(100);
+        uint32_t after_freq = ledc_get_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_1);
+        display(0,"SERIAL_ISOLATION_TEST(%d) before(%u) after(%u) rslt(%d)",
+            SERIAL_ISOLATION_TEST,
+            before_freq,
+            after_freq,
+            rslt);
     #endif
 
     display(0,"TE3.ino setup() finished",0);
